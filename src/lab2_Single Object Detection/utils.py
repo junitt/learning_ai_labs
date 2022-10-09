862c94a295599ce3fbdbe7facda2b2c932e13896
junitt <2058067865@qq.com>
2022-10-09 19:45:53 +0800

    upd utils

--- src/lab2_Single Object Detection/utils.py
+++ src/lab2_Single Object Detection/utils.py
@@ -5,7 +5,15 @@ import torch
 def compute_iou(bbox1, bbox2):
 
     # TODO Compute IoU of 2 bboxes.
-
+    s1=(bbox1[:,2]-bbox1[:,0])*(bbox1[:,3]-bbox1[:,1])
+    s2=(bbox2[:,2]-bbox2[:,0])*(bbox2[:,3]-bbox2[:,1])
+    a=np.maximum(bbox1[:,0],bbox2[:,0])
+    b=np.maximum(bbox1[:,1],bbox2[:,1])
+    c=np.minimum(bbox1[:,2],bbox2[:,2])
+    d=np.minimum(bbox1[:,3],bbox2[:,3])
+    op=np.maximum((c-a),0)*np.maximum((d-b),0)
+    sm=s1+s2-op
+    return op/sm
     ...
 
     # End of todo
