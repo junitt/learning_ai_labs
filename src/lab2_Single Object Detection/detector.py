4f9b6f0d81b3cd0f311c12dab88bdbb96319e53d
junitt <2058067865@qq.com>
2022-10-10 21:45:37 +0800

    finished

--- src/lab2_Single Object Detection/detector.py
+++ src/lab2_Single Object Detection/detector.py
@@ -1,10 +1,41 @@
+from turtle import forward
 import torch.nn as nn
 import resnet
 
 
 # TODO Design the detector.
 # tips: Use pretrained `resnet` as backbone.
+class NeuralNetwork(nn.Module):
+    def __init__(self, lengths, num_classes):
+        super().__init__()
+        lst=[]
+        for i in range(len(lengths)-1):
+            lst.append(nn.Linear(lengths[i],lengths[i+1]))
+            lst.append(nn.ReLU())
+        lst1=lst
+        lst2=lst
+        lst1.append(nn.Linear(lengths[-1],num_classes))
+        self.cls_pred=nn.Sequential(*tuple(lst1))
+        lst.pop()
+        lst2.append(nn.Linear(lengths[-1],4))
+        self.bbox_pred=nn.Sequential(*tuple(lst2))
+    def forward(self,x):
+        logits=self.cls_pred(x)
+        bbox=self.bbox_pred(x)
+        return logits,bbox
 
+
+class Detector(nn.Module):
+    def __init__(self, backbone, lengths, num_classes):
+        super().__init__()
+        self.backbone=getattr(resnet,backbone)(pretrained=True)
+        self.mynet=NeuralNetwork(lengths, num_classes)
+        self.flat=nn.Flatten()
+    def forward(self,x):
+        x=self.backbone(x)
+        x=self.flat(x)
+        logits,bbox=self.mynet(x)
+        return logits,bbox
 ...
 
 # End of todo
--- src/lab2_Single Object Detection/main.py
+++ src/lab2_Single Object Detection/main.py
@@ -10,8 +10,8 @@ from utils import compute_iou
 
 
 lr = 5e-3
-batch = 32
-epochs = 100
+batch = 10
+epochs = 34
 device = "cuda" if torch.cuda.is_available() else "cpu"
 iou_thr = 0.5
 
@@ -25,14 +25,19 @@ def train_epoch(model, dataloader, criterion: dict, optimizer,
     for X, y in bar:
 
         # TODO Implement the train pipeline.
-
+        x,y_cls,y_bbox=X.to(device),y['cls'].to(device),y['box'].to(device)
+        logits,bbox=model(x)
+        loss=criterion['cls'](logits,y_cls)+5*criterion['box'](bbox,y_bbox)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        correct+=sum((torch.argmax(logits,axis=1)==y_cls).cpu().detach().numpy()&(compute_iou(bbox.cpu(),y_bbox.cpu())>=iou_thr))
+        total+=len(X)
         ...
 
         # End of todo
 
-        bar.set_postfix_str(f'lr={scheduler.get_last_lr()[0]:.4f}'
-                            ' acc={correct / total * 100:.2f}'
-                            ' loss={loss.item():.2f}')
+        bar.set_postfix_str(f'lr={scheduler.get_last_lr()[0]:.4f} acc={correct / total * 100:.2f} loss={loss.item():.2f}')
     scheduler.step()
 
 
@@ -43,7 +48,11 @@ def test_epoch(model, dataloader, device, epoch):
         for X, y in dataloader:
 
             # TODO Implement the test pipeline.
-
+            x,y_cls,y_bbox=X.to(device),y['cls'].to(device),y['box'].to(device)
+            logits,bbox=model(x)
+            correct+=sum((torch.argmax(logits,axis=1)==y_cls).cpu().detach().numpy()&(compute_iou(bbox.cpu(),y_bbox.cpu())>=iou_thr))
+            total+=len(X)
+            correct_cls+=sum(torch.argmax(logits.cpu(),axis=1)==y_cls.cpu())
             ...
 
             # End of todo
@@ -52,9 +61,9 @@ def test_epoch(model, dataloader, device, epoch):
 
 
 def main():
-    trainloader = data.DataLoader(TvidDataset(root='~/data/tiny_vid', mode='train'),
+    trainloader = data.DataLoader(TvidDataset(root='./data/tiny_vid', mode='train'),#修改了下路径
                                   batch_size=batch, shuffle=True, num_workers=4)
-    testloader = data.DataLoader(TvidDataset(root='~/data/tiny_vid', mode='test'),
+    testloader = data.DataLoader(TvidDataset(root='./data/tiny_vid', mode='test'),
                                  batch_size=batch, shuffle=True, num_workers=4)
     model = Detector(backbone='resnet50', lengths=(2048 * 4 * 4, 2048, 512),
                      num_classes=5).to(device)
